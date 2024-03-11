8fe870e483dca61baeb4ce32625b76eee34782f2
ethanolchik <eitan.olchik@gmail.com>
2024-03-11 11:18:50 +0000

    base add functionality for form

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-// Get all elements with class "socials-icon"
 var socialIcons = document.querySelectorAll('.socials-icon');
-
-// Add click event listener to each social icon
 socialIcons.forEach(function(icon) {
     icon.addEventListener('click', function() {
-        // Check the alt attribute value of the clicked social icon
         var altValue = this.getAttribute('alt');
 
-        // Redirect based on the alt attribute value
         if (altValue === 'GitHub') {
-            window.location.href = 'https://github.com/ethanolchik'; // Redirect to GitHub
+            window.location.href = 'https://github.com/ethanolchik';
         } else if (altValue === 'LinkedIn') {
-            window.location.href = 'https://www.linkedin.com/in/ethan-olchik-65b4282a5/'; // Redirect to LinkedIn
+            window.location.href = 'https://www.linkedin.com/in/ethan-olchik-65b4282a5/';
         }
-        // Add more conditions for other social media if needed
     });
 });
 
-const textElement = document.querySelector('.about-description');
 
+const textElement = document.querySelector('.about-description');
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.intersectionRatio > 0) {
@@ -26,9 +20,8 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 }, {
-    root: null, // Use the viewport as the root element
-    threshold: 0 // Trigger the callback when any part of the element is visible
+    root: null,
+    threshold: 0
 });
 
 observer.observe(textElement);
-
---
+++ submit.php
@@ -0,0 +1,29 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  // Set recipient email address
+  $to = "your_email@example.com";
+
+  // Get form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  // Prepare email content
+  $subject = "Portfolio form Submission from $name";
+  $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+  // Set email headers (optional)
+  $headers = "From: $email \r\n";
+  $headers .= "Reply-To: $email \r\n";
+  $headers .= "Content-Type: text/plain; charset=UTF-8 \r\n";
+
+  // Send email
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Your message has been sent successfully!";
+  } else {
+    echo "There was an error sending your message. Please try again later.";
+  }
+}
+
+?>
